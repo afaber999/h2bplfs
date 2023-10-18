44483f4c966ce5378ce1c634b14205920a263c08
nly96021 <albert.faber@philips.com>
2023-10-18 12:35:05 +0200

    test scopes

---
+++ src/ep_05/frontend/lexer.rs
@@ -0,0 +1,133 @@
+// hkindps://github.com/tlaceby/guide-to-interpreters-series
+// -----------------------------------------------------------
+// ---------------          LEXER          -------------------
+// ---  Responsible for producing tokens from the source   ---
+// -----------------------------------------------------------
+
+// Represents tokens that our language understands in parsing.
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum TokenKind {
+	// Literal Types
+    Null,
+	Number,
+	Identifier,
+
+	// Keywords
+	Let,
+
+	// Grouping * Operators
+	BinaryOperator,
+	Equals,
+	OpenParen,
+	CloseParen,    
+
+    // Other
+    Eof,
+}
+
+
+#[derive(Debug)]
+pub struct Token {
+    pub value : String,
+    pub kind  : TokenKind,
+}
+
+impl Token {
+    pub fn new(value : String, kind : TokenKind) -> Self {
+        Self {
+            value,
+            kind,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Keyword {
+    text : &'static str,
+    kind   : TokenKind,
+}
+
+impl Keyword {
+    pub const fn new(text: &'static str, kind : TokenKind) -> Self {
+        Self {
+            text,
+            kind,
+        }
+    } 
+}
+
+static KEYWORDS : [Keyword; 2] = [ Keyword::new( "let", TokenKind::Let),Keyword::new( "null", TokenKind::Null)];
+
+fn is_integer(x: &char) -> bool {
+    x.is_numeric()
+}
+
+fn is_alphanumeric(x: &char) -> bool {
+    let extra_chars = "_.";
+    x.is_alphanumeric() || extra_chars.contains(*x)
+}
+
+
+#[derive(Debug)]
+pub struct Lexer {}
+
+impl Lexer {
+
+    pub fn new() -> Self {Self {}}
+
+    pub fn tokenize(src_code : &String) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        let mut src = src_code.chars().peekable();
+        
+        // build each token until end of file
+        while let Some(c) = src.next() {
+
+            let mut value = c.to_string();
+
+            match c {
+                '(' => tokens.push(Token::new(value,TokenKind::OpenParen)),
+                ')' => tokens.push(Token::new(value,TokenKind::CloseParen)),
+                '+' | '-' | '*' | '/' | '%'  => tokens.push(Token::new(value,TokenKind::BinaryOperator)),
+                '=' => tokens.push(Token::new(value,TokenKind::Equals)),
+
+                ' ' | '\t' | '\n' | '\r' => {
+                    // ignore whitespace
+                }
+                _ => {
+                    if c.is_numeric() {
+                        while let Some(c) = src.next_if(is_integer) {
+                            value.push(c);
+                        }
+                        tokens.push(Token::new(value,TokenKind::Number))
+                    } else  if c.is_alphabetic() {
+                        while let Some(c) = src.next_if(is_alphanumeric) {
+                            value.push(c);
+                        }
+
+                        // check for reserve keywords
+                        KEYWORDS.iter().for_each(|k| {
+                            if k.text == &value {
+                                println!("Special keywoard: {:?}", k.kind);
+                                tokens.push(Token::new(k.text.to_string(),k.kind));
+                                value.clear();                                
+                            }
+                        });
+
+                        if value.len() > 0 {
+                            tokens.push(Token::new(value,TokenKind::Identifier))
+                        }
+                    } else {
+                        panic!("Can't tokenize character '{}'", c);
+                    }
+                } 
+            }
+        }
+        
+        // indicate end of tokens
+        tokens.push(Token::new("".to_string(),TokenKind::Eof));
+        tokens
+    }
+
+}
+
---
+++ src/ep_05/main.rs
@@ -0,0 +1,68 @@
+use std::io::{self, Write};
+
+pub mod frontend;
+pub mod runtime;
+
+use frontend::parser::Parser;
+use runtime::interpreter::Interpreter;
+
+use crate::runtime::environment::Environment;
+
+fn repl() -> io::Result<()> {
+    let mut interpreter = Interpreter::new();
+    let mut environment = Environment::new();
+    let global_scope = environment.add_scope(0);
+    let scope2 = environment.add_scope(global_scope);
+    let scope3 = environment.add_scope(scope2);
+    let scope4 = environment.add_scope(global_scope);
+
+
+    environment.declare_variable(global_scope, "poop", runtime::values::RtValue::NumberVal(4.0));
+    environment.declare_variable(global_scope, "pi", runtime::values::RtValue::NumberVal(3.14159267));
+    environment.declare_variable(global_scope, "poopie", runtime::values::RtValue::NumberVal(4.0));
+    environment.declare_variable(scope4, "poopie1", runtime::values::RtValue::NumberVal(4.0));
+
+
+    environment.list_scope(global_scope);
+    environment.list_scope(scope2);
+    environment.list_scope(scope3);
+    environment.list_scope(scope4);
+
+
+    println!("Get value 1 {:?}", environment.get_value(global_scope, "pi"));
+    println!("Get value 2 {:?}", environment.get_value(global_scope, "two_pi"));
+    println!("Get value 3 {:?}", environment.get_value(global_scope, "poop"));
+
+    environment.assign_value(scope3, "poop", runtime::values::RtValue::NumberVal(12.0));
+    println!("Get value 3 {:?}", environment.get_value(scope3, "poop"));
+
+    environment.declare_variable(scope3, "poop", runtime::values::RtValue::NumberVal(13.0));
+
+    println!("Get value 4 {:?}", environment.get_value(global_scope, "poop"));
+    println!("Get value 5 {:?}", environment.get_value(scope3, "poop"));
+
+    println!("Repl v0.15");
+    //let src_code = "let x = ( 10 + 5 ) * 2".to_string();
+    loop {
+        print!(">");
+        std::io::stdout().flush()?;
+
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        if buffer.starts_with("exit") {
+            break;
+        }
+
+        let program = Parser::produce_ast(&buffer);
+        println!("{:?}", program);
+        
+        let rtval = interpreter.evaluate(&program);
+        println!("{:?}", rtval);
+
+    }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    return repl();
+}
